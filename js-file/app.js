8a90ebef889c3fdf02a5fcc4a00231d576261503
AkashMAbrrar <mabrrar9@gmail.com>
2022-02-17 15:16:15 +0600

    this commit for javascript dom and money calculation

--- js-file/app.js
+++ js-file/app.js
@@ -1,1 +1,23 @@
-console.log('hello from javascript')
+document.getElementById('btn-calculation').addEventListener('click', function () {
+    const foodCost = parseFloat(document.getElementById('food-input').value);
+    const rentCost = parseFloat(document.getElementById('rent-input').value);
+    const clothsCost = parseFloat(document.getElementById('cloths-input').value);
+    const totalCost = foodCost + rentCost + clothsCost;
+    // 
+    const totalExpense = document.getElementById('total-expese').innerText = totalCost;
+    // 
+    const totalIncome = document.getElementById('income-input');
+    const incomeAmount = parseFloat(totalIncome.value);
+
+    const incomeExpese = incomeAmount - totalExpense;
+
+
+
+    //
+
+
+
+    console.log(incomeExpese);
+
+    // console.log(foodInput);
+})
